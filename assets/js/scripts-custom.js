429fe35db690d620ffe5acf411e40b124723de28
abdohantash <abdohantash9999@gmail.com>
2023-01-10 23:58:06 +0200

    style

--- assets/js/scripts-custom.js
+++ assets/js/scripts-custom.js
@@ -31,6 +31,42 @@ $(document).on('change', '.phone-part .form-check-input', function () {
     }   
 });
 
+$(document).on('change', '.lname-part .form-check-input', function () {
+    if (this.checked){
+        $(this).closest('.lname-part').find('.lname-ul input').prop("type", "text")
+    }
+    else {
+        $(this).closest('.lname-part').find('.lname-ul input').prop("type", "password")
+    }   
+});
+
+$(document).on('change', '.birth-part .form-check-input', function () {
+    if (this.checked){
+        $(this).closest('.birth-part').find('.birth-ul input').prop("type", "text")
+    }
+    else {
+        $(this).closest('.birth-part').find('.birth-ul input').prop("type", "password")
+    }   
+});
+
+$(document).on('change', '.email-part .form-check-input', function () {
+    if (this.checked){
+        $(this).closest('.email-part').find('.email-ul input').prop("type", "text")
+    }
+    else {
+        $(this).closest('.email-part').find('.email-ul input').prop("type", "password")
+    }   
+});
+
+$(document).on('change', '.address-part .form-check-input', function () {
+    if (this.checked){
+        $(this).closest('.address-part').find('.address-ul input').prop("type", "text")
+    }
+    else {
+        $(this).closest('.address-part').find('.address-ul input').prop("type", "password")
+    }   
+});
+
 
 // super agent active
 $(document).on('click', '.agentbox ul li', function () {
