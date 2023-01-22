9e041ba92d492aea100149b9c0f421ece3e82f3d
abdohantash <abdohantash9999@gmail.com>
2023-01-22 20:50:00 +0200

    style

--- assets/js/scripts-custom.js
+++ assets/js/scripts-custom.js
@@ -13,59 +13,59 @@ $(document).on('click', '.close-menu,.overlay-menu', function () {
 });
 
 // toggle btn
-$(document).on('change', '.certificate .form-check-input', function () {
-    if (this.checked){
-        $(this).closest('.certificate').find('.certificate-ul input').prop("type", "text")
-    }
-    else {
-        $(this).closest('.certificate').find('.certificate-ul input').prop("type", "password")
-    }   
-});
+// $(document).on('change', '.certificate .form-check-input', function () {
+//     if (this.checked){
+//         $(this).closest('.certificate').find('.certificate-ul input').prop("type", "text")
+//     }
+//     else {
+//         $(this).closest('.certificate').find('.certificate-ul input').prop("type", "password")
+//     }   
+// });
 
-$(document).on('change', '.phone-part .form-check-input', function () {
-    if (this.checked){
-        $(this).closest('.phone-part').find('.phone-ul input').prop("type", "text")
-    }
-    else {
-        $(this).closest('.phone-part').find('.phone-ul input').prop("type", "password")
-    }   
-});
+// $(document).on('change', '.phone-part .form-check-input', function () {
+//     if (this.checked){
+//         $(this).closest('.phone-part').find('.phone-ul input').prop("type", "text")
+//     }
+//     else {
+//         $(this).closest('.phone-part').find('.phone-ul input').prop("type", "password")
+//     }   
+// });
 
-$(document).on('change', '.lname-part .form-check-input', function () {
-    if (this.checked){
-        $(this).closest('.lname-part').find('.lname-ul input').prop("type", "text")
-    }
-    else {
-        $(this).closest('.lname-part').find('.lname-ul input').prop("type", "password")
-    }   
-});
+// $(document).on('change', '.lname-part .form-check-input', function () {
+//     if (this.checked){
+//         $(this).closest('.lname-part').find('.lname-ul input').prop("type", "text")
+//     }
+//     else {
+//         $(this).closest('.lname-part').find('.lname-ul input').prop("type", "password")
+//     }   
+// });
 
-$(document).on('change', '.birth-part .form-check-input', function () {
-    if (this.checked){
-        $(this).closest('.birth-part').find('.birth-ul input').prop("type", "text")
-    }
-    else {
-        $(this).closest('.birth-part').find('.birth-ul input').prop("type", "password")
-    }   
-});
+// $(document).on('change', '.birth-part .form-check-input', function () {
+//     if (this.checked){
+//         $(this).closest('.birth-part').find('.birth-ul input').prop("type", "text")
+//     }
+//     else {
+//         $(this).closest('.birth-part').find('.birth-ul input').prop("type", "password")
+//     }   
+// });
 
-$(document).on('change', '.email-part .form-check-input', function () {
-    if (this.checked){
-        $(this).closest('.email-part').find('.email-ul input').prop("type", "text")
-    }
-    else {
-        $(this).closest('.email-part').find('.email-ul input').prop("type", "password")
-    }   
-});
+// $(document).on('change', '.email-part .form-check-input', function () {
+//     if (this.checked){
+//         $(this).closest('.email-part').find('.email-ul input').prop("type", "text")
+//     }
+//     else {
+//         $(this).closest('.email-part').find('.email-ul input').prop("type", "password")
+//     }   
+// });
 
-$(document).on('change', '.address-part .form-check-input', function () {
-    if (this.checked){
-        $(this).closest('.address-part').find('.address-ul input').prop("type", "text")
-    }
-    else {
-        $(this).closest('.address-part').find('.address-ul input').prop("type", "password")
-    }   
-});
+// $(document).on('change', '.address-part .form-check-input', function () {
+//     if (this.checked){
+//         $(this).closest('.address-part').find('.address-ul input').prop("type", "text")
+//     }
+//     else {
+//         $(this).closest('.address-part').find('.address-ul input').prop("type", "password")
+//     }   
+// });
 
 
 // super agent active
